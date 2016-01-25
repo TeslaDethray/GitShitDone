7315be525ba1528e780fcd73e0feb40bc2886a1e
tesladethray <tesladethray@gmail.com>
2016-01-24 23:21:44 -0800

    Added OtganizationCollection

---
+++ src/AppBundle/Model/Collections/OrganizationCollection.php
@@ -0,0 +1,9 @@
+<?php
+
+// src/AppBundle/Model/Collection/OrganizationCollection.php
+namespace AppBundle\Model\Collection;
+
+use AppBundle\Model\Collection\DefaultCollection;
+
+class OrganizationCollection extends DefaultCollection {
+}
